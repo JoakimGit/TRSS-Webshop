ae70d50d18ae9d6e02cb5a88a996c6b0b8a5059e
joal <jokke34@gmail.com>
2020-12-20 22:24:14 +0100

    Added search buttons to 'see all' views.

--- src/main/java/dat19c/kea/trsswebshop/Model/Employee.java
+++ src/main/java/dat19c/kea/trsswebshop/Model/Employee.java
@@ -13,6 +13,7 @@ public class Employee {
     private Long employee_id;
     private String firstName;
     private String lastName;
+    private String username;
     private String email;
     private String phoneNumber;
     private String hireDate;
@@ -42,6 +43,14 @@ public class Employee {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
@@ -74,9 +83,10 @@ public class Employee {
                 "employee_id=" + employee_id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
                 ", email='" + email + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
-                ", hireDate=" + hireDate +
+                ", hireDate='" + hireDate + '\'' +
                 '}';
     }
 }
--- src/main/java/dat19c/kea/trsswebshop/Model/Invoice.java
+++ src/main/java/dat19c/kea/trsswebshop/Model/Invoice.java
@@ -1,4 +1,63 @@
 package dat19c.kea.trsswebshop.Model;
 
+import javax.persistence.*;
+
+@Entity
 public class Invoice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long invoice_id;
+    private String invoice_date;
+    private String status;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Invoice() {
+    }
+
+    public Long getInvoice_id() {
+        return invoice_id;
+    }
+
+    public void setInvoice_id(Long invoice_id) {
+        this.invoice_id = invoice_id;
+    }
+
+    public String getInvoice_date() {
+        return invoice_date;
+    }
+
+    public void setInvoice_date(String invoice_date) {
+        this.invoice_date = invoice_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "invoice_id=" + invoice_id +
+                ", invoice_date='" + invoice_date + '\'' +
+                ", status='" + status + '\'' +
+                ", order=" + order +
+                '}';
+    }
 }
